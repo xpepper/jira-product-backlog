825c6c5ee0aa835f00c181a0f3be83012678a9e9
matteo.vaccari <matteo.vaccari@a76cf5f7-6f51-0410-b198-77c9135f6fd8>
2008-07-09 17:59:49 +0000

    created report class

---
+++ lib/report.rb
@@ -0,0 +1,24 @@
+class Report
+  
+  def initialize(backlog=nil)
+    @backlog = backlog
+  end
+  
+  def row_for(team)
+    all_stories = @backlog.number_of_stories_for(team)
+    not_estimated = @backlog.not_yet_estimated_for(team)
+    total_points = @backlog.total_of_all_for(team)
+    points_earned = @backlog.total_done_for(team)
+    points_remaining = total_points - points_earned
+    [team, points_remaining, "", total_points, points_earned, all_stories, not_estimated].join("\t")
+  end
+  
+  def header
+    "Points remaining\tProjection\tTotal points\tPoints earned\tNumber of stories\tNon-estimated stories"
+  end
+  
+  def report(*teams)
+    rows = teams.map {|t| row_for(t)}
+    header + "\n" + rows.join("\n")
+  end
+end
