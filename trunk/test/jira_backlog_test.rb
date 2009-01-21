d7e1e18aa6594623230f1d698fafc9e573144c3b
pierodibello <pierodibello@a76cf5f7-6f51-0410-b198-77c9135f6fd8>
2009-01-21 15:45:01 +0000

    updated the project to calculate the backlog for the STORM project

--- trunk/test/jira_backlog_test.rb
+++ trunk/test/jira_backlog_test.rb
@@ -8,47 +8,39 @@ class JiraBacklogTest < Test::Unit::TestCase
   @@backlog = JiraBacklog.new(File.join(File.dirname(__FILE__), "test.xml"))
 
   def test_ker_227
-    story = @@backlog["KER-227"]
+    story = @@backlog["STORM-167"]
     assert_not_nil story
-    assert_equal "KER-227", story.key
-    assert_equal "[KER-227] LSE Beta Products - About The Exchange", story.title
+    assert_equal "STORM-167", story.key
+    assert_equal "[STORM-167] Freeze/unfreeze sprint", story.title
     assert_equal "Open", story.status
-    assert_equal 10, story.estimated_complexity
+    assert_equal 15, story.estimated_complexity
     assert_equal Fixnum, story.estimated_complexity.class
-    assert_equal "IPO", story.team
+    assert_equal "Agile Planning Tool", story.team
   end
     
   def test_story_count_totals
-    assert_equal 18, @@backlog.stories.size
-    assert_equal 5,  @@backlog.all_story_of("PRICES") .size
-    
-    assert_equal 3, @@backlog.not_yet_estimated_for("PRICES")
-    assert_equal 12, @@backlog.total_done_for("PRICES")
-    assert_equal 47 + 12, @@backlog.total_of_all_for("PRICES")
+    assert_equal 160, @@backlog.stories.size
+    assert_equal 160,  @@backlog.all_story_of("Agile Planning Tool").size
+        
+    assert_equal 25, @@backlog.not_yet_estimated_for("Agile Planning Tool")    
+    assert_equal 51, @@backlog.total_done_for("Agile Planning Tool")
+        
+    assert_equal 252,  @@backlog.in_status_for("Open", "Agile Planning Tool")
+    assert_equal 3,  @@backlog.in_status_for("Resolved", "Agile Planning Tool")
+    assert_equal 10,  @@backlog.in_status_for("Reopened", "Agile Planning Tool")
+    assert_equal 51,  @@backlog.in_status_for("Closed", "Agile Planning Tool")
+    assert_equal 252 + 3 + 10 + 51, @@backlog.total_of_all_for("Agile Planning Tool")
 
-    
-    assert_equal 10,  @@backlog.all_story_of("IPO") .size
-    assert_equal 9, @@backlog.not_yet_estimated_for("IPO")
-    assert_equal 10, @@backlog.total_of_all_for("IPO")
-   
-    assert_equal 3,  @@backlog.all_story_of("NEWS") .size
-    
-    assert_equal 0,  @@backlog.in_status_for("Internal Signoff", "NEWS")
-    assert_equal 12,  @@backlog.in_status_for("Internal Signoff", "PRICES")
+    assert_equal 0,  @@backlog.in_status_for("Open", "unknown component")
   end
   
   def test_null_complexity
-    story = @@backlog["KER-209"]
+    story = @@backlog["STORM-25"]
     assert_nil story.estimated_complexity
   end
   
-  def test_resolution_date
-    story = @@backlog["KER-209"]
-    assert_equal "2008-06-18", story.resolution_date
-  end
-
   def test_number_of_stories_for
-    assert_equal 10, @@backlog.number_of_stories_for("IPO")
+    assert_equal 160, @@backlog.number_of_stories_for("Agile Planning Tool")
   end
 
 end
